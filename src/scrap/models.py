06bce90da2babc83a6a3ecb59aa97b8f7870844b
Andrewakiv <danchyshy.andriy@gmail.com>
2023-11-12 15:53:55 +0200

    start temp

--- src/scrap/models.py
+++ src/scrap/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Language'
+        verbose_name_plural = 'Languages'
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Vacancy'
+        verbose_name_plural = 'Vacancies'
+
+    def __str__(self):
+        return self.title
--- src/scrap/views.py
+++ src/scrap/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Vacancy
+from .forms import FindForm
 
-# Create your views here.
+
+def home_view(request):
+    vacancies = Vacancy.objects.all()
+
+    if request.method == 'POST':
+        form = FindForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # print(cd)
+            vacancies = Vacancy.objects.filter(city__name=cd['city'], language__name=cd['language'])
+            # print(vacancies)
+    else:
+        form = FindForm()
+
+    context = {
+        'object_list': vacancies,
+        'form': form
+    }
+
+    return render(request, 'scrap/vacancies/home.html', context=context)
