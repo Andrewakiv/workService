ff2e2d3fdf6e1405abf2e7a1cb37f058f41aeab6
Andrewakiv <danchyshy.andriy@gmail.com>
2023-12-08 13:44:52 +0200

    filters to search

--- src/scrap/views.py
+++ src/scrap/views.py
@@ -6,12 +6,19 @@ from .forms import FindForm
 def home_view(request):
     vacancies = Vacancy.objects.all()
 
-    if request.method == 'POST':
-        form = FindForm(request.POST)
+    if request.method == 'GET':
+        form = FindForm(request.GET)
         if form.is_valid():
             cd = form.cleaned_data
             # print(cd)
-            vacancies = Vacancy.objects.filter(city__name=cd['city'], language__name=cd['language'])
+            if cd['city'] and cd['language']:
+                vacancies = Vacancy.objects.filter(city__name=cd['city'], language__name=cd['language'])
+            elif cd['language']:
+                vacancies = Vacancy.objects.filter(language__name=cd['language'])
+            elif cd['city']:
+                vacancies = Vacancy.objects.filter(city__name=cd['city'])
+            else:
+                vacancies = Vacancy.objects.all()
             # print(vacancies)
     else:
         form = FindForm()
