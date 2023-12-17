c9b31d49c4673a7f3d1e343b4b4c57d126290720
Andrewakiv <danchyshy.andriy@gmail.com>
2023-12-17 12:28:49 +0200

    mails

---
+++ src/send_mail.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from django.core.mail import EmailMultiAlternatives
+from django.contrib.auth import get_user_model
+import django
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "work_service.settings"
+
+django.setup()
+from scrap.models import Vacancy
+from work_service.settings import EMAIL_HOST_USER
+
+User = get_user_model()
+
+subject = 'Hot vacancies'
+text_content = 'hot vacancies'
+from_email = EMAIL_HOST_USER
+empty = 'There are not new vacancies for your filters'
+
+qs = User.objects.filter(send_mail=True).values('city', 'language', 'email')
+print('qs', qs)
+user_dct = {}
+for i in qs:
+    user_dct.setdefault((i["city"], i['language']), [])
+    user_dct[(i["city"], i['language'])].append(i['email'])
+print("user_dct", user_dct)
+if user_dct:
+    params = {'city_id__in': [], 'language_id__in': []}
+    for pair in user_dct.keys():
+        params['city_id__in'].append(pair[0])
+        params['language_id__in'].append(pair[1])
+    print('params', params)
+    qs = Vacancy.objects.filter(**params).values()[:10]
+    print('qs', qs)
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i["city_id"], i['language_id']), [])
+        vacancies[(i["city_id"], i['language_id'])].append(i)
+    print("vacancies", vacancies)
+    for keys, emails in user_dct.items():
+        rows = vacancies.get(keys, [])
+        html = ''
+        for row in rows:
+            html += f'<h3><a href="{ row["url"] }">{ row["title"] }</a></h5>'
+            html += f"<p>{row['description']}</p>"
+            html += f"<p>{row['company']}</p><br><hr>"
+        _html = html if html else empty
+        for email in emails:
+            to = email
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(_html, "text/html")
+            msg.send()
+
--- src/work_service/settings.py
+++ src/work_service/settings.py
@@ -126,3 +126,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.MyUser"
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
